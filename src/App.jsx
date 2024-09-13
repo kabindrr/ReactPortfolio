2d6f009ade344e2c9eef4b59817dce59cd014b12
kabindrr <chaina_kabindra@yahoo.com>
2024-09-13 22:19:49 +1000

    add react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
       <h1>Hello developers </h1>
+      <Button className="btn btn-success">Submit</Button>
     </>
   );
 };
