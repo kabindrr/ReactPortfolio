8ee300aebcf039aec6cad8564a632d1b903c3de0
kabindrr <chaina_kabindra@yahoo.com>
2024-09-16 22:03:04 +1000

    updated projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,8 @@ import project3 from "../assets/project3.png";
 import project4 from "../assets/project4.png";
 import project5 from "../assets/project5.png";
 import project6 from "../assets/project6.png";
+import project7 from "../assets/project7.png";
+
 import { Card } from "./Card";
 import { Title } from "./Title";
 
@@ -24,32 +26,39 @@ export const Projects = () => {
       description: "TechStack: Html, Css, Figma, Bootstrap",
     },
     {
+      title: "Travel Blog",
+      image: project1,
+      github: "https://github.com/kabindrr/PersonalBlog.git",
+      url: "https://personal-blog-ashy.vercel.app/",
+      description: "TechStack: Html, Css, Figma",
+    },
+    {
       title: "Solar System",
-      image: { project5 },
+      image: project5,
       github: "https://github.com/kabindrr/SolarSystem.git",
       url: "https://solar-system-topaz.vercel.app",
       description: "TechStack: Html, Css, Figma",
     },
     {
-      title: "Solar System",
-      image: { project5 },
-      github: "https://github.com/kabindrr/SolarSystem.git",
-      url: "https://solar-system-topaz.vercel.app",
+      title: "Contact List",
+      image: project3,
+      github: "https://github.com/kabindrr/RandomContactList.git",
+      url: "https://random-contact-list-det6achv5-kabindras-projects.vercel.app",
       description: "TechStack: Html, Css, Figma",
     },
     {
-      title: "Solar System",
-      image: { project5 },
-      github: "https://github.com/kabindrr/SolarSystem.git",
-      url: "https://solar-system-topaz.vercel.app",
+      title: "Calculator",
+      image: project6,
+      github: "https://github.com/kabindrr/JsCalculator2.git",
+      url: "https://js-calculator2-gbv4.vercel.app",
       description: "TechStack: Html, Css, Figma",
     },
     {
-      title: "Solar System",
-      image: { project5 },
-      github: "https://github.com/kabindrr/SolarSystem.git",
-      url: "https://solar-system-topaz.vercel.app",
-      description: "TechStack: Html, Css, Figma",
+      title: "Js QUIZ",
+      image: project7,
+      github: "https://github.com/kabindrr/JsQuiz.git",
+      url: "https://js-quiz-o4ir.vercel.app",
+      description: "TechStack: Html, Css, JavaScript",
     },
   ];
   return (
@@ -62,109 +71,6 @@ export const Projects = () => {
             {projects.map((item, i) => (
               <Card key={i} {...item} />
             ))}
-            <div className="project-card container">
-              <div className="top">
-                <img src={project4} alt="" width="100%" />
-              </div>
-              <div className=" flex bottom container">
-                <div className="links">
-                  <a href="https://github.com/kabindrr/Beer-count-app.git">
-                    <i
-                      className="fa-brands fa-github"
-                      style={{ color: "gray" }}
-                    ></i>{" "}
-                  </a>
-
-                  <a href="https://beer-count-app.vercel.app">
-                    <i
-                      className="fa-brands fa-chrome"
-                      style={{ color: "yellow" }}
-                    ></i>
-                  </a>
-                </div>
-                <h3>Count Your Beer</h3>
-                <p>TechStack: Html, Css, Figma, JavaScript</p>
-              </div>
-            </div>
-            <div className="project-card container">
-              <div className="top">
-                <img src={project1} alt="" width="100%" />
-              </div>
-              <div className=" flex bottom container">
-                <div className="links">
-                  <a href="https://github.com/kabindrr/PersonalBlog.git">
-                    <i
-                      className="fa-brands fa-github"
-                      style={{ color: "gray" }}
-                    ></i>
-                  </a>
-
-                  <a href="https://personal-blog-ashy.vercel.app/">
-                    <i
-                      className="fa-brands fa-chrome"
-                      style={{ color: "yellow" }}
-                    ></i>
-                  </a>
-                </div>
-                <h3>Trave Blog</h3>
-                <p>TechStack: Html, Css, Figma</p>
-              </div>
-            </div>
-            <div className="project-card container">
-              <div className="top">
-                <img src={project3} alt="" width="100%" />
-              </div>
-              <div className=" flex bottom container">
-                <div className="links">
-                  <a href="https://github.com/kabindrr/RandomContactList.git">
-                    <i
-                      className="fa-brands fa-github"
-                      style={{ color: "gray" }}
-                    ></i>
-                  </a>
-
-                  <a href="https://random-contact-list-three.vercel.app/">
-                    <i
-                      className="fa-brands fa-chrome"
-                      style={{ color: "yellow" }}
-                    ></i>
-                  </a>
-                </div>
-                <h3>Random Contact List</h3>
-                <p>TechStack: Html, Css, Figma, JavaScript</p>
-              </div>
-            </div>
-            <div className="project-card container">
-              <div className="top">
-                <img src={project6} alt="" width="100%" />
-              </div>
-              <div className=" flex bottom container">
-                <div className="links">
-                  <a href="https://github.com/kabindrr/ReactPrankCalculator.git">
-                    <i
-                      className="fa-brands fa-github"
-                      style={{ color: "gray" }}
-                    ></i>
-                  </a>
-
-                  <a href="https://react-prank-calculator-tau.vercel.app/">
-                    <i
-                      className="fa-brands fa-chrome"
-                      style={{ color: "yellow" }}
-                    ></i>
-                  </a>
-                </div>
-                <h3>Prank Calculator</h3>
-                <p>TechStack: Html, Css, Figma, JavaScript</p>
-              </div>
-            </div>
-            <Card
-              title="Solar System"
-              image={project5}
-              github="https://github.com/kabindrr/SolarSystem.git"
-              url="https://solar-system-topaz.vercel.app"
-              description="TechStack: Html, Css, Figma"
-            />
           </div>
         </div>
       </section>
